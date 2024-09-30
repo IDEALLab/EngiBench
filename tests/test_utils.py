7cd37df70ff337c9f7a63e9b3891dc02832ecab3
Florian Felten <felten.florian@hotmail.fr>
2024-09-30 15:03:46 +0200

    fix tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -19,13 +19,13 @@ def test_clone_template() -> None:
     # Tests the replacement
     with open(study_dir + "/template_1.py") as f:
         content = f.read()
-    assert content == "hello = hello\nworld = world\nprint(hello, world)"
+    assert content == "hello = hello\nworld = world\nprint(hello, world)\n"
 
     # Another file, in another format
     replace_template_values(study_dir + "/template_2.yml", {"hi": "hello", "world": "world"})
     with open(study_dir + "/template_2.yml") as f:
         content = f.read()
-    assert content == "hi: hello\nworld: world"
+    assert content == "hi: hello\nworld: world\n"
 
     # Cleanup
     os.remove(study_dir + "/template_1.py")
