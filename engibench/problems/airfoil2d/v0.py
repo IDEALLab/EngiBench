01273ef0a1c37c9d84d8262042dc3c8673730950
cashend <cashen.diniz123@gmail.com>
2025-01-28 12:37:34 -0500

    Fixed airfoil coordinate processing.  Temporarily disabled ruff checking of some future TODOs.

--- engibench/problems/airfoil2d/v0.py
+++ engibench/problems/airfoil2d/v0.py
@@ -164,6 +164,131 @@ class Airfoil2D(Problem[str, npt.NDArray]):
         )
         return filename
 
+    def __reorder_coords(self, df_slice: pd.DataFrame) -> npt.NDArray:  # noqa: PLR0915
+        """Reorder the coordinates of a slice such that the segments are ordered correctly. WIP temporary code.
+
+        Args:
+            df_slice (pd.DataFrame): The raw dataframe slice.
+            return_indicies (bool): If True, returns the reordered indices of the slice, instead of the coordinates.
+
+        Returns:
+            coords_reordered (np.ndarray): The reordered x and y coordinates of the slice.
+        """
+        # Now get connectivities
+        node_c1 = np.array(df_slice['NodeC1'].dropna().values).astype(int) # A list of [1,2,3,4,...]
+        node_c2 = np.array(df_slice['NodeC2'].dropna().values).astype(int) # A list of [2,3,4,5,...]
+        connectivities = np.concatenate((node_c1.reshape(-1, 1), node_c2.reshape(-1, 1)), axis=1) # A list of [[1,2],[2,3],[3,4],...]
+
+        # plot the x 'CoordinateX' and y 'CoordinateY' coordinates of the slice
+        coords_x = df_slice['CoordinateX'].values
+        coords_y = df_slice['CoordinateY'].values
+
+        # We also have XoC YoC ZoC VelocityX VelocityY VelocityZ CoefPressure Mach
+        # We would like to reorder these values in the same way as the coordinates, so we keep track of the indices
+        indicies = np.arange(len(df_slice))
+        id_breaks_start_id = [0]
+        id_breaks_end_id = []
+        prev_id = 0
+        segment_ids = np.zeros(len(connectivities))
+        seg_id = 0
+
+        for j in range(len(connectivities)):
+            if connectivities[j][0] - 1 != prev_id:
+                # This means that we have a new set of points
+                id_breaks_start_id.append(connectivities[j][0]-1)
+                id_breaks_end_id.append(prev_id)
+                seg_id += 1
+            segment_ids[j] = seg_id
+
+            prev_id = connectivities[j][1] - 1
+
+        id_breaks_end_id.append(j)
+
+        unique_segment_ids = np.arange(seg_id+1)
+        new_seg_order = unique_segment_ids.copy()
+
+        # Loop over and sort the segments such that the end of each x and y coordinate for each segment is the start of the next segment
+        # Loop through the segment ids
+        seg_coords_start_x = coords_x[id_breaks_start_id]
+        seg_coords_start_y = coords_y[id_breaks_start_id]
+        seg_coords_end_x = coords_x[id_breaks_end_id]
+        seg_coords_end_y = coords_y[id_breaks_end_id]
+
+        err = 1e-8
+        for j in range(len(unique_segment_ids)):
+            seg_coords_start_x_j = seg_coords_start_x[j]
+            seg_coords_start_y_j = seg_coords_start_y[j]
+            seg_coords_end_x_j = seg_coords_end_x[j]
+            seg_coords_end_y_j = seg_coords_end_y[j]
+            # Loop through the segment ids
+            seg_x_diff_start = np.abs(seg_coords_start_x_j - seg_coords_end_x)
+            seg_y_diff_start = np.abs(seg_coords_start_y_j - seg_coords_end_y)
+            seg_tot_diff_start = seg_x_diff_start + seg_y_diff_start
+            seg_tot_diff_min_id_start = np.argmin(seg_tot_diff_start)
+            seg_tot_diff_min_start = seg_tot_diff_start[seg_tot_diff_min_id_start]
+
+            seg_x_diff_end = np.abs(seg_coords_end_x_j - seg_coords_start_x)
+            seg_y_diff_end = np.abs(seg_coords_end_y_j - seg_coords_start_y)
+            seg_tot_diff_end = seg_x_diff_end + seg_y_diff_end
+            seg_tot_diff_min_id_end = np.argmin(seg_tot_diff_end)
+
+            new_seg_order_temp = unique_segment_ids.copy()
+
+            if seg_tot_diff_min_start > err:
+                # No segment matches, so we have found the starting segment
+                # Now we need to reorder the segments
+                # Put the first segment in the first position
+                new_seg_order_temp[0] = unique_segment_ids[j]
+                new_seg_order_temp[j] = unique_segment_ids[0]
+                # Now choose the id that is minimum; i.e we match the end of the first segment to the start of the next most likely segment
+                new_seg_order_temp[1] = unique_segment_ids[seg_tot_diff_min_id_end]
+                new_seg_order_temp[seg_tot_diff_min_id_end] = unique_segment_ids[1]
+                # Now loop through the remaining segments and match the end of the previous segment to the start of the next most likely segment, breaking if we reach the end
+                for k in range(2,len(unique_segment_ids)):
+                    # Get the previous segment id
+                    prev_seg_id = new_seg_order_temp[k-1]
+                    # Get the previous segment end coordinates
+                    prev_seg_end_x = seg_coords_end_x[prev_seg_id]
+                    prev_seg_end_y = seg_coords_end_y[prev_seg_id]
+                    # Get the remaining segment ids
+                    remaining_seg_ids = np.setdiff1d(unique_segment_ids, new_seg_order_temp[:k])
+                    # Get the remaining segment start coordinates
+                    remaining_seg_start_x = seg_coords_start_x[remaining_seg_ids]
+                    remaining_seg_start_y = seg_coords_start_y[remaining_seg_ids]
+                    # Get the remaining segment end coordinates
+                    # Get the difference between the previous segment end coordinates and the remaining segment start coordinates
+                    remaining_seg_x_diff = np.abs(prev_seg_end_x - remaining_seg_start_x)
+                    remaining_seg_y_diff = np.abs(prev_seg_end_y - remaining_seg_start_y)
+                    remaining_seg_tot_diff = remaining_seg_x_diff + remaining_seg_y_diff
+                    # Get the minimum difference
+                    remaining_seg_tot_diff_min_id = np.argmin(remaining_seg_tot_diff)
+                    # Now get the id of the remaining segment that matches the previous segment end coordinates
+                    remaining_seg_id = remaining_seg_ids[remaining_seg_tot_diff_min_id]
+                    # Now put the remaining segment id in the next position
+                    new_seg_order_temp[k] = remaining_seg_id
+
+                # Now we have a new segment order
+                new_seg_order = new_seg_order_temp
+                break
+
+        # We can use the new order to plot all segments at once
+        # Concatenate the segments in the new order
+        coords_x_reordered = np.array([])
+        coords_y_reordered = np.array([])
+        indicies_reordered = np.array([])
+
+        for j in range(len(new_seg_order)):
+            segment = np.nonzero(segment_ids == new_seg_order[j])[0]
+            coords_x_segment = coords_x[connectivities[segment]-1][:,0]
+            coords_y_segment = coords_y[connectivities[segment]-1][:,0]
+            indicies_segment = indicies[connectivities[segment]-1][:,0]
+            coords_x_reordered = np.concatenate((coords_x_reordered, coords_x_segment))
+            coords_y_reordered = np.concatenate((coords_y_reordered, coords_y_segment))
+            indicies_reordered = np.concatenate((indicies_reordered, indicies_segment))
+
+        return np.array([coords_x_reordered, coords_y_reordered])
+
+
     def __simulator_output_to_design(self, simulator_output: str | None = None) -> npt.NDArray:
         """Converts a simulator input to a design.
 
@@ -204,13 +329,12 @@ class Airfoil2D(Problem[str, npt.NDArray]):
 
         # Read the main data and node connections
         slice_df = pd.read_csv(slice_file, sep=r"\s+", names=var_names, skiprows=5, nrows=nnodes, engine="c")
-        # TODO Cashen is this necessary to extract the shape?
-        #  nodes_arr = pd.read_csv(slice_file, sep=r"\s+", names=["NodeC1", "NodeC2"], skiprows=5 + nnodes, engine="c")
+        nodes_arr = pd.read_csv(slice_file, sep=r"\s+", names=["NodeC1", "NodeC2"], skiprows=5 + nnodes, engine="c")
 
         # Concatenate node connections to the main data
-        # slice_df = pd.concat([slice_df, nodes_arr], axis=1)
+        slice_df = pd.concat([slice_df, nodes_arr], axis=1)
 
-        design = slice_df[["CoordinateX", "CoordinateY"]].values.transpose()
+        design = self.__reorder_coords(slice_df)
         return design
 
     def simulate(self, design: npt.NDArray, config: dict[str, Any] = {}, mpicores: int = 4) -> npt.NDArray:
@@ -236,7 +360,7 @@ class Airfoil2D(Problem[str, npt.NDArray]):
             "temperature": 223.150,  # should specify either mach + altitude or mach + reynolds + reynoldsLength (default to 1) + temperature
             "output_dir": "'" + self.__docker_study_dir + "/output/'",
             "mesh_fname": "'" + self.__docker_study_dir + "/design.cgns'",
-            "task": "'analysis'",  # TODO: We can add the option to perform a polar analysis.
+            "task": "'analysis'",  # TODO: We can add the option to perform a polar analysis.  # noqa: FIX002
         }
 
         base_config.update(self.boundary_conditions)
@@ -314,7 +438,7 @@ class Airfoil2D(Problem[str, npt.NDArray]):
         optisteps_history = []
         history = pyoptsparse.History(self.__local_study_dir + "/output/opt.hst")
 
-        # TODO return the full history of the optimization instead of just the last step
+        # TODO return the full history of the optimization instead of just the last step  # noqa: FIX002
         # Also, this is inconsistent with the definition of the problem saying we optimize 2 objectives...
         objective = history.getValues(names=["obj"], callCounters=None, allowSens=False, major=False, scale=True)["obj"][
             -1, -1
@@ -351,7 +475,7 @@ class Airfoil2D(Problem[str, npt.NDArray]):
         Returns:
             DesignType: The valid random design.
         """
-        rnd = self.np_random.integers(low=0, high=len(self.dataset["train"]["initial"]))  # pyright: ignore[reportOptionalMemberAccess]
+        rnd = self.np_random.integers(low=0, high=len(self.dataset["train"]["initial"]))  # pyright: ignore[reportArgumentType, reportCallIssue, reportOptionalMemberAccess]
         return np.array(self.dataset["train"]["initial"][rnd])  # type: ignore
 
 
@@ -368,7 +492,3 @@ if __name__ == "__main__":
         "airfoil.png",
         dpi=300,
     )
-    # config_keys = dataset["train"].features.keys() - ["initial", "optimized"]
-    # config = {key: dataset["train"][key][0] for key in config_keys}
-    #
-    # print(problem.optimize(design, config=config, mpicores=8))
