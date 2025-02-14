7d4ef152017cb2d2529ecfa35921f9ddec007308
arthurdrake1 <adrake17@umd.edu>
2025-02-13 23:38:21 -0500

    hide params from user and fix random_design method

--- engibench/problems/beams2d/v0.py
+++ engibench/problems/beams2d/v0.py
@@ -13,7 +13,6 @@ from gymnasium import spaces
 import numpy as np
 import numpy.typing as npt
 
-from engibench.core import DesignType
 from engibench.core import OptiStep
 from engibench.core import Problem
 from engibench.problems.beams2d.backend import calc_sensitivity
@@ -84,6 +83,7 @@ class Beams2D(Problem[npt.NDArray, npt.NDArray]):
     design_space = spaces.Box(low=0.0, high=1.0, shape=(5000,), dtype=np.float32)
     dataset_id = "IDEALLab/beams_2d_v0"
     _dataset = None
+    __p = None
     container_id = None  # type: ignore
 
     def __init__(self) -> None:
@@ -92,87 +92,85 @@ class Beams2D(Problem[npt.NDArray, npt.NDArray]):
 
         self.seed = None
 
-    def simulate(
-        self, design: npt.NDArray, p: Params | None = None, ce: npt.NDArray | None = None, config: dict[str, Any] = {}
-    ) -> npt.NDArray:
+    def simulate(self, design: npt.NDArray, ce: npt.NDArray | None = None, config: dict[str, Any] = {}) -> npt.NDArray:
         """Simulates the performance of a beam design.
 
         Args:
             design (np.ndarray): The design to simulate.
-            p: Params object all needed values, vectors, and matrices needed for the simulation.
             ce: (np.ndarray, optional): If applicable, the pre-calculated sensitivity of the current design.
             config (dict): A dictionary with configuration (e.g., boundary conditions) for the simulation.
 
         Returns:
             npt.NDArray: The performance of the design in terms of compliance.
         """
-        if p is None:
-            p = Params()
+        if self.__p is None:
+            self.__p = Params()
             base_config = {}  # No specification for base_config since it is equivalent to self.boundary_conditions
             base_config.update(self.boundary_conditions)
             base_config.update(config)
-            p.update(base_config)
-            p = setup(p)
+            self.__p.update(base_config)
+            self.__p = setup(self.__p)
 
         if ce is None:
-            ce = calc_sensitivity(design, p)
-        c = ((p.Emin + design**p.penal * (p.Emax - p.Emin)) * ce).sum()  # compliance (objective)
+            ce = calc_sensitivity(design, self.__p)
+        c = (
+            (self.__p.Emin + design**self.__p.penal * (self.__p.Emax - self.__p.Emin)) * ce
+        ).sum()  # compliance (objective)
         return np.array(c)
 
-    def optimize(self, p: Params | None = None, config: dict[str, Any] = {}) -> tuple[np.ndarray, list[OptiStep]]:
+    def optimize(self, config: dict[str, Any] = {}) -> tuple[np.ndarray, list[OptiStep]]:
         """Optimizes the design of a beam.
 
         Args:
-            p: Params object all needed values, vectors, and matrices needed for the optimization.
             config (dict): A dictionary with configuration (e.g., boundary conditions) for the optimization.
 
         Returns:
             Tuple[np.ndarray, dict]: The optimized design and its performance.
         """
         # Prepares the optimization script/function with the optimization configuration
-        if p is None:
-            p = Params()
+        if self.__p is None:
+            self.__p = Params()
             base_config = {}  # No specification for base_config since it is equivalent to self.boundary_conditions
             base_config.update(self.boundary_conditions)
             base_config.update(config)
-            p.update(base_config)
-            p = setup(p)
+            self.__p.update(base_config)
+            self.__p = setup(self.__p)
 
         # Make sure to include the intermediate designs of size (5000,)
         # Make sure to return the full history of the optimization instead of just the last step
         optisteps_history = []
 
-        dc = np.zeros(p.nely * p.nelx)
-        dv = np.zeros(p.nely * p.nelx)
-        ce = np.ones(p.nely * p.nelx)
+        dc = np.zeros(self.__p.nely * self.__p.nelx)
+        dv = np.zeros(self.__p.nely * self.__p.nelx)
+        ce = np.ones(self.__p.nely * self.__p.nelx)
 
-        xPhys = x = p.volfrac * np.ones(p.nely * p.nelx, dtype=float)
-        xPrint, _, _ = overhang_filter(xPhys, p, dc, dv)
+        xPhys = x = self.__p.volfrac * np.ones(self.__p.nely * self.__p.nelx, dtype=float)
+        xPrint, _, _ = overhang_filter(xPhys, self.__p, dc, dv)
 
         loop, change = (0, 1)
 
-        while change > p.min_change and loop < p.max_iter:
+        while change > self.__p.min_change and loop < self.__p.max_iter:
             loop = loop + 1
 
-            ce = calc_sensitivity(xPrint, p=p)
-            c = self.simulate(xPrint, p=p, ce=ce)
+            ce = calc_sensitivity(xPrint, p=self.__p)
+            c = self.simulate(xPrint, ce=ce)
 
-            dc = (-p.penal * xPrint ** (p.penal - 1) * (p.Emax - p.Emin)) * ce
-            dv = np.ones(p.nely * p.nelx)
-            xPrint, dc, dv = overhang_filter(xPhys, p, dc, dv)  # MATLAB implementation
+            dc = (-self.__p.penal * xPrint ** (self.__p.penal - 1) * (self.__p.Emax - self.__p.Emin)) * ce
+            dv = np.ones(self.__p.nely * self.__p.nelx)
+            xPrint, dc, dv = overhang_filter(xPhys, self.__p, dc, dv)  # MATLAB implementation
 
-            if p.ft == 0:
-                dc = np.asarray((p.H * (x * dc))[np.newaxis].T / p.Hs)[:, 0] / np.maximum(0.001, x)  # type: ignore
-            elif p.ft == 1:
-                dc = np.asarray(p.H * (dc[np.newaxis].T / p.Hs))[:, 0]
-                dv = np.asarray(p.H * (dv[np.newaxis].T / p.Hs))[:, 0]
+            if self.__p.ft == 0:
+                dc = np.asarray((self.__p.H * (x * dc))[np.newaxis].T / self.__p.Hs)[:, 0] / np.maximum(0.001, x)  # type: ignore
+            elif self.__p.ft == 1:
+                dc = np.asarray(self.__p.H * (dc[np.newaxis].T / self.__p.Hs))[:, 0]
+                dv = np.asarray(self.__p.H * (dv[np.newaxis].T / self.__p.Hs))[:, 0]
 
             # Optimality criteria
             l1, l2, move = (0, 1e9, 0.2)
             # reshape to perform vector operations
-            xnew = np.zeros(p.nelx * p.nely)
+            xnew = np.zeros(self.__p.nelx * self.__p.nely)
 
-            while l1 + l2 > 0 and (l2 - l1) / (l1 + l2) > p.min_ratio:
+            while l1 + l2 > 0 and (l2 - l1) / (l1 + l2) > self.__p.min_ratio:
                 lmid = 0.5 * (l2 + l1)
                 if lmid > 0:
                     xnew = np.maximum(
@@ -182,20 +180,22 @@ class Beams2D(Problem[npt.NDArray, npt.NDArray]):
                     xnew = np.maximum(0.0, np.maximum(x - move, np.minimum(1.0, x + move)))
 
                 # Filter design variables
-                if p.ft == 0:
+                if self.__p.ft == 0:
                     xPhys = xnew
-                elif p.ft == 1:
-                    xPhys = np.asarray(p.H * xnew[np.newaxis].T / p.Hs)[:, 0]
+                elif self.__p.ft == 1:
+                    xPhys = np.asarray(self.__p.H * xnew[np.newaxis].T / self.__p.Hs)[:, 0]
 
-                xPrint, _, _ = overhang_filter(xPhys, p, dc, dv)
+                xPrint, _, _ = overhang_filter(xPhys, self.__p, dc, dv)
 
-                if xPrint.sum() > p.volfrac * p.nelx * p.nely:
+                if xPrint.sum() > self.__p.volfrac * self.__p.nelx * self.__p.nely:
                     l1 = lmid
                 else:
                     l2 = lmid
 
             # Compute the change by the inf. norm
-            change = np.linalg.norm(xnew.reshape(p.nelx * p.nely, 1) - x.reshape(p.nelx * p.nely, 1), np.inf)
+            change = np.linalg.norm(
+                xnew.reshape(self.__p.nelx * self.__p.nely, 1) - x.reshape(self.__p.nelx * self.__p.nely, 1), np.inf
+            )
             x = deepcopy(xnew)
 
             # Record the current state in optisteps_history
@@ -213,6 +213,7 @@ class Beams2D(Problem[npt.NDArray, npt.NDArray]):
             **kwargs: Additional keyword arguments.
         """
         super().reset(seed, **kwargs)
+        self.__p = None
 
     def render(self, design: np.ndarray, nelx: int = 100, nely: int = 50, open_window: bool = False) -> Any:
         """Renders the design in a human-readable format.
@@ -236,19 +237,16 @@ class Beams2D(Problem[npt.NDArray, npt.NDArray]):
             plt.show()
         return fig, ax
 
-    def random_design(self, designs: np.ndarray) -> tuple[DesignType, int]:
+    def random_design(self) -> tuple[np.ndarray, int]:
         """Samples a valid random design.
-
-        Args:
-            designs (np.ndarray): The set of possible designs to choose from.
 
         Returns:
             Tuple of:
-                DesignType: The valid random design.
+                np.ndarray: The valid random design.
                 int: The random index selected.
         """
-        rnd = self.np_random.integers(low=0, high=designs.shape[0])  # type: ignore
-        return (designs[rnd], rnd)
+        rnd = self.np_random.integers(low=0, high=len(self._dataset["train"]), dtype=int)  # type: ignore
+        return (np.array(self._dataset["train"]["xPrint"][rnd]), rnd)  # type: ignore
 
 
 if __name__ == "__main__":
@@ -258,41 +256,35 @@ if __name__ == "__main__":
     dataset = problem.dataset
 
     # Example of getting the training set
-    xPrint_train = np.array(dataset["train"]["xPrint"])  # type: ignore
-    c_train = np.array(dataset["train"]["compliance"])  # type: ignore
-    params_train = np.array(dataset["train"].remove_columns(["xPrint", "compliance"]))  # type: ignore
-
-    print("Shape of xPrint_train:", xPrint_train.shape)
+    xPrint_train = dataset["train"]["xPrint"]  # type: ignore
+    c_train = dataset["train"]["compliance"]  # type: ignore
+    params_train = dataset["train"].remove_columns(["xPrint", "compliance"])  # type: ignore
 
     # Get design and conditions from the dataset, render design
-    design, idx = problem.random_design(xPrint_train)
+    design, idx = problem.random_design()
     config = params_train[idx]
-    config["nelx"] = 200
-    config["nely"] = 100
     compliance = c_train[idx]
-    fig, ax = problem.render(design, nelx=200, nely=100, open_window=True)
+    nelx = config["nelx"]
+    nely = config["nely"]
+    fig, ax = problem.render(design, nelx=nelx, nely=nely, open_window=True)
     fig.savefig(
         "beam_random.png",
         dpi=300,
     )
 
-    # The compliance calculated here will be slightly different if the original nelx and nely were different
-    # All designs were upscaled to 100x200 prior to uploading to HF
-    print("Verifying compliance via simulation. Reference value:", compliance)
+    print(f"Verifying compliance via simulation. Reference value: {compliance:.4f}")
 
     try:
         c_ref = problem.simulate(design, config=config)
-        print("Calculated compliance:", c_ref)
+        print(f"Calculated compliance: {c_ref:.4f}")
     except ArithmeticError:
         print("Failed to calculate compliance for upscaled design.")
 
     # Sample Optimization
-    print("\nNow conducting a sample optimization with the provided configs.")
+    print("\nNow conducting a sample optimization with the given configs:", config)
 
     # NOTE: xPrint and optisteps_history[-1].stored_design are interchangeable.
-    xPrint, optisteps_history = problem.optimize()
-    nelx = dict(problem.boundary_conditions)["nelx"]
-    nely = dict(problem.boundary_conditions)["nely"]
+    xPrint, optisteps_history = problem.optimize(config=config)
     print(f"Final compliance: {optisteps_history[-1].obj_values[0]:.4f}")
     print(
         f"Final design volume fraction: {xPrint.sum() / (nelx*nely):.4f}"  # type: ignore
