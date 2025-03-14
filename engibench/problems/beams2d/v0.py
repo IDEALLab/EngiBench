f29e78cfb61d1f8ce2518ef6b792ce31fa48d83c
arthurdrake1 <adrake17@umd.edu>
2025-03-14 18:08:39 +0100

    automatically flatten design in optimize as needed

--- engibench/problems/beams2d/v0.py
+++ engibench/problems/beams2d/v0.py
@@ -162,6 +162,8 @@ class Beams2D(Problem[npt.NDArray, npt.NDArray]):
             dc = np.zeros(self.__p.nely * self.__p.nelx)
             dv = np.zeros(self.__p.nely * self.__p.nelx)
         else:
+            if len(design.shape) > 1:
+                design = image_to_design(design)
             xPhys = x = deepcopy(design)
             ce = calc_sensitivity(design, p=self.__p)
             dc = (-self.__p.penal * design ** (self.__p.penal - 1) * (self.__p.Emax - self.__p.Emin)) * ce
