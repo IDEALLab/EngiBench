74f6f24ddd831d0b30ab617f1ce8b64670a3cf99
liangXD523 <xuliangd0523@gmail.com>
2025-03-18 20:05:51 -0400

    update the docstring of ``plot`` and ``rowdtype``; remove print()

--- engibench/problems/power_electronics/utils/raw_read.py
+++ engibench/problems/power_electronics/utils/raw_read.py
@@ -76,8 +76,6 @@ def rawread(fname: str) -> tuple[np.ndarray, dict]:
         except:
             raise
 
-        print(f"rawread.py: mdata = {mdata}, len = {len(mdata)}, {fp.tell()}")
-
         if len(mdata) == 2:
             # print("mdata len = 2", mdata)
             if mdata[0].lower() in MDATA_LIST:
@@ -112,17 +110,15 @@ def rawread(fname: str) -> tuple[np.ndarray, dict]:
 
                     plot["varnames"].append(vname_)  # "i(v_gs3)", "v(gs4)", "gain"
                     plot["varunits"].append(vunit)  # "current", "voltage", "notype"
-
-                """ An example of the variable plot:
+                    """ An example of the variable {plot}:
                     {b'title': b'* rewrite netlist',
-                    b'date': b'Wed Feb 12 00:58:17  2025',
+                    b'date': b'Tue Mar 18 19:55:46  2025',
                     b'plotname': b'Transient Analysis',
                     b'flags': b'real',
-                    b'no. variables': b'62',
+                    b'no. variables': b'59',
                     b'no. points': b'12001',
-                    'varnames': ['time', 'i(@c0[i])', 'i(@c1[i])', 'i(@c2[i])', 'i(@c3[i])', 'i(@c4[i])', 'i(@c5[i])', 'i(@d0[i])', 'i(@d1[i])', 'i(@d2[i])', 'i(@d3[i])', 'i(@l0[i])', 'i(@l1[i])', 'i(@l2[i])', 'i(@r0[i])', 'i(@r0[i])', 'i(@r0[i])', 'i(@r0[i])', 'i(@rc0[i])', 'i(@rc1[i])', 'i(@rc2[i])', 'i(@rc3[i])', 'i(@rc4[i])', 'i(@rc5[i])', 'i(@s0[i])', 'i(@s1[i])', 'i(@s2[i])', 'i(@s3[i])', 'i(@s4[i])', 'v(1)', 'v(2)', 'v(3)', 'v(4)', 'v(5)', 'v(6)', 'v(7)', 'v(8)', 'v(9)', 'v(10)', 'gain', 'gs0', 'gs1', 'gs2', 'gs3', 'gs4', 'gs_ref_d', 'gs_ref_dc', 'i(l0)', 'i(l1)', 'i(l2)', 'i(v0)', 'i(v_gs0)', 'i(v_gs1)', 'i(v_gs2)', 'i(v_gs3)', 'i(v_gs4)', 'i(v_gsref_d)', 'i(v_gsref_dc)', 'vdiff', 'vo_mean', 'vpp', 'vpp_ratio'],
-                    'varunits': ['time', 'current', 'current', 'current', 'current', 'current', 'current', 'current', 'current', 'current', 'current', 'current', 'current', 'current', 'current', 'current', 'current', 'current', 'current', 'current', 'current', 'current', 'current', 'current', 'current', 'current', 'current', 'current', 'current', 'voltage', 'voltage', 'voltage', 'voltage', 'voltage', 'voltage', 'voltage', 'voltage', 'voltage', 'voltage', 'notype', 'voltage', 'voltage', 'voltage', 'voltage', 'voltage', 'voltage', 'voltage', 'current', 'current', 'current', 'current', 'current', 'current', 'current', 'current', 'current', 'current', 'current', 'notype', 'notype', 'notype', 'notype']}
-                """
+                    'varnames': ['time', 'i(@c0[i])', 'i(@c1[i])', 'i(@c2[i])', 'i(@c3[i])', 'i(@c4[i])', 'i(@c5[i])', 'i(@d0[i])', 'i(@d1[i])', 'i(@d2[i])', 'i(@d3[i])', 'i(@l0[i])', 'i(@l1[i])', 'i(@l2[i])', 'i(@r0[i])', 'i(@rc0[i])', 'i(@rc1[i])', 'i(@rc2[i])', 'i(@rc3[i])', 'i(@rc4[i])', 'i(@rc5[i])', 'i(@s0[i])', 'i(@s1[i])', 'i(@s2[i])', 'i(@s3[i])', 'i(@s4[i])', 'v(1)', 'v(2)', 'v(3)', 'v(4)', 'v(5)', 'v(6)', 'v(7)', 'v(8)', 'v(9)', 'v(10)', 'gain', 'v(gs0)', 'v(gs1)', 'v(gs2)', 'v(gs3)', 'v(gs4)', 'v(gs_ref_d)', 'v(gs_ref_dc)', 'i(l0)', 'i(l1)', 'i(l2)', 'i(v0)', 'i(v_gs0)', 'i(v_gs1)', 'i(v_gs2)', 'i(v_gs3)', 'i(v_gs4)', 'i(v_gsref_d)', 'i(v_gsref_dc)', 'vdiff', 'vo_mean', 'vpp', 'vpp_ratio'],
+                    'varunits': ['time', 'current', 'current', 'current', 'current', 'current', 'current', 'current', 'current', 'current', 'current', 'current', 'current', 'current', 'current', 'current', 'current', 'current', 'current', 'current', 'current', 'current', 'current', 'current', 'current', 'current', 'voltage', 'voltage', 'voltage', 'voltage', 'voltage', 'voltage', 'voltage', 'voltage', 'voltage', 'voltage', 'notype', 'voltage', 'voltage', 'voltage', 'voltage', 'voltage', 'voltage', 'voltage', 'current', 'current', 'current', 'current', 'current', 'current', 'current', 'current', 'current', 'current', 'current', 'notype', 'notype', 'notype', 'notype']}"""
 
             if mdata[0].lower() == b"binary":
                 # print("binary content:", mdata[1])
@@ -130,40 +126,33 @@ def rawread(fname: str) -> tuple[np.ndarray, dict]:
                 names = plot["varnames"]
                 formats = [np.complex_ if b"complex" in plot[b"flags"] else np.float64] * nvars
 
-                print("names", names)
-                print("formats", formats)
-
-                rowdtype = np.dtype({"names": names, "formats": formats})
-                # rowdtype = np.dtype(
-                #     {
-                #         "names": plot["varnames"],
-                #         "formats": [
-                #             np.complex_
-                #             if b"complex" in plot[b"flags"]
-                #             #  else np.float_]*nvars})
-                #             # An error happens when testing under Ubuntu 20:
-                #             # AttributeError: `np.float_` was removed in the NumPy 2.0 release. Use `np.float64` instead.
-                #             else np.float64
-                #         ]
-                #         * nvars,
-                #     }
-                # )
-                print("rowdtype", rowdtype)  # structured type dtype. https://numpy.org/doc/2.1/user/basics.rec.html
+                rowdtype = np.dtype(
+                    {"names": names, "formats": formats}
+                )  # structured type dtype. https://numpy.org/doc/2.1/user/basics.rec.html
                 """Expected rowdtype:
-               todo """
+                [('time', '<f8'), ('i(@c0[i])', '<f8'), ('i(@c1[i])', '<f8'), ('i(@c2[i])', '<f8'), ('i(@c3[i])', '<f8'),
+                ('i(@c4[i])', '<f8'), ('i(@c5[i])', '<f8'), ('i(@d0[i])', '<f8'), ('i(@d1[i])', '<f8'), ('i(@d2[i])', '<f8'),
+                ('i(@d3[i])', '<f8'), ('i(@l0[i])', '<f8'), ('i(@l1[i])', '<f8'), ('i(@l2[i])', '<f8'), ('i(@r0[i])', '<f8'),
+                ('i(@rc0[i])', '<f8'), ('i(@rc1[i])', '<f8'), ('i(@rc2[i])', '<f8'), ('i(@rc3[i])', '<f8'), ('i(@rc4[i])', '<f8'),
+                ('i(@rc5[i])', '<f8'), ('i(@s0[i])', '<f8'), ('i(@s1[i])', '<f8'), ('i(@s2[i])', '<f8'), ('i(@s3[i])', '<f8'),
+                ('i(@s4[i])', '<f8'), ('v(1)', '<f8'), ('v(2)', '<f8'), ('v(3)', '<f8'), ('v(4)', '<f8'), ('v(5)', '<f8'),
+                ('v(6)', '<f8'), ('v(7)', '<f8'), ('v(8)', '<f8'), ('v(9)', '<f8'), ('v(10)', '<f8'), ('gain', '<f8'),
+                ('v(gs0)', '<f8'), ('v(gs1)', '<f8'), ('v(gs2)', '<f8'), ('v(gs3)', '<f8'), ('v(gs4)', '<f8'), ('v(gs_ref_d)', '<f8'),
+                ('v(gs_ref_dc)', '<f8'), ('i(l0)', '<f8'), ('i(l1)', '<f8'), ('i(l2)', '<f8'), ('i(v0)', '<f8'), ('i(v_gs0)', '<f8'),
+                ('i(v_gs1)', '<f8'), ('i(v_gs2)', '<f8'), ('i(v_gs3)', '<f8'), ('i(v_gs4)', '<f8'), ('i(v_gsref_d)', '<f8'),
+                ('i(v_gsref_dc)', '<f8'), ('vdiff', '<f8'), ('vo_mean', '<f8'), ('vpp', '<f8'), ('vpp_ratio', '<f8')]
+                """
                 # We should have all the metadata by now
                 arrs.append(np.fromfile(fp, dtype=rowdtype, count=npoints))
-                print(f"Retrieved the values. np.fromfile() changed the pointer to {fp.tell()}.")
-                print("arrs", arrs)  # This looks better in jupyter notebook.
-                print("arrs[0].shape", arrs[0].shape)
+                # print(f"Retrieved the values. np.fromfile() changed the pointer to {fp.tell()}.")
+                # print("arrs", arrs)  # This looks better in jupyter notebook.
+                # print("arrs[0].shape", arrs[0].shape)
                 plots.append(plot)
 
                 fp.readline()  # Move the pointer to the end of line.
-                # print(f"Then readline() changed the pointer to {fp.tell()}.")
-                # print(fp.readline(), "\n")
 
         else:
-            print(f"length of mdata = {len(mdata)}. Break the iteration.")
+            # print(f"length of mdata = {len(mdata)}. Break the iteration.")
             break
 
     return (arrs[0], plots[0])
