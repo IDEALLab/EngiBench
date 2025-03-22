62b5b87809277536e838fe9a8c4bb8587f2b181a
liangXD523 <xuliangd0523@gmail.com>
2025-03-21 22:20:24 -0400

    Add docstrings and type hints. Rename class names.

--- engibench/problems/power_electronics/utils/component.py
+++ engibench/problems/power_electronics/utils/component.py
@@ -1,8 +1,11 @@
 """@Author: Naga Siva Srinivas Putta <nagasiva@umd.edu>."""
 
 
-class Simulation_params:
-    def __init__(self, params):
+class SimulationParams:
+    """Simulation parameters for power electronics components. LTSpice syntax."""
+
+    def __init__(self, params: dict[str, float]):
+        """Initialize simulation parameters."""
         self.Tstart = params.get("Tstart")
         self.Tstop = params.get("Tstop")
         self.Tstep = params.get("Tstep")
@@ -11,35 +14,48 @@ class Simulation_params:
 
 
 class Resistor:
-    def __init__(self, params):
+    """Resistor parameters for power electronics simulation."""
+
+    def __init__(self, params: dict[str, float]):
+        """Initialize simulation parameters."""
         self.R = params.get("Resistance")
 
 
 class Capacitor:
-    def __init__(self, params):
+    """Capacitor parameters for power electronics simulation."""
+
+    def __init__(self, params: dict[str, float]):
+        """Initialize simulation parameters."""
         self.C = params.get("Capacitance")
         self.ESR = params.get("ESR")
         self.Rp = params.get("Parallel Resistance")
 
 
 class Diode:
-    def __init__(self, params):
+    """Diode parameters for power electronics simulation."""
+
+    def __init__(self, params: dict[str, float]):
+        """Initialize simulation parameters."""
         self.Rd = params.get("Dynamic Resistance")
         self.Vt0 = params.get("Threshold Voltage")
         self.Qrr = params.get("Qrr")
 
 
 class Inductor:
-    def __init__(self, params):
+    """Inductor parameters for power electronics simulation."""
+
+    def __init__(self, params: dict[str, float]):
+        """Initialize simulation parameters."""
         self.L = params.get("Inductance")
         self.DCR = params.get("DCR")
 
 
 class MOSFET:
-    def __init__(self, params):
+    """MOSFET parameters for power electronics simulation."""
+
+    def __init__(self, params: dict[str, float]):
+        """Initialize simulation parameters."""
         self.Ron = params.get("R_DS_on")
-        # self.Qrr = params.get("MOSFET_Qrr")
-        # self.trr = params.get("MOSFET_trr")
         self.Coss_vs_Vds = params.get("Coss vs Vds")
         self.td_on = params.get("Turn-on delay time")
         self.tr = params.get("Rise time")
@@ -48,6 +64,9 @@ class MOSFET:
         self.Q_g_tot = params.get("Gate charge total")
 
 
-class DC_Voltage_Source:
-    def __init__(self, params):
+class DCVoltageSource:
+    """DC Voltage source for power electronics simulation."""
+
+    def __init__(self, params: dict[str, float]):
+        """Initialize simulation parameters."""
         self.V_dc = params.get("DC Voltage")
