f9d5061c7bdf58e65c47023857574888b5e06083
liangXD523 <xuliangd0523@gmail.com>
2025-03-29 23:15:26 -0400

    change self.cmp_edg_str in rewrite() using string format f{}

--- engibench/problems/power_electronics/v0.py
+++ engibench/problems/power_electronics/v0.py
@@ -159,6 +159,7 @@ class PowerElectronics(Problem[npt.NDArray]):
         self.netlist_name = (
             self.original_netlist_path.replace("\\", "/").split("/")[-1].removesuffix(".net")
         )  # python 3.9 and newer
+        self.rewrite_netlist_path = os.path.join(self.netlist_dir, f"rewrite_{self.netlist_name}.net")
         self.log_file_path = os.path.normpath(os.path.join(self.log_file_dir, f"{self.netlist_name}.log"))
         self.raw_file_path = os.path.normpath(os.path.join(self.raw_file_dir, f"{self.netlist_name}.raw"))
 
@@ -247,7 +248,9 @@ class PowerElectronics(Problem[npt.NDArray]):
         It creates the direct input file sent to ngSpice.
         The main difference between this rewrite.netlist and the original netlist is the control section that contains simulation parameters.
         """
-        self.rewrite_netlist_path = os.path.join(self.netlist_dir, f"rewrite_{mode}_{self.netlist_name}.net")
+        self.rewrite_netlist_path = os.path.join(
+            self.netlist_dir, f"rewrite_{mode}_{self.netlist_name}.net"
+        )  # add {mode} compared to that in __load_netlist()
         print(f"rewriting netlist to: {self.rewrite_netlist_path}")
 
         with open(self.rewrite_netlist_path, "w") as file:
@@ -255,92 +258,29 @@ class PowerElectronics(Problem[npt.NDArray]):
 
             RC_str = ""
             for i in range(len(self.capacitor_val)):
-                RC_str = (
-                    RC_str
-                    + "RC"
-                    + str(i)
-                    + " "
-                    + str(self.edge_map["C" + str(i)][0])
-                    + " "
-                    + str(self.edge_map["C" + str(i)][1])
-                    + " 100meg\n"
-                )
+                RC_str += f"RC{i} {self.edge_map[f'C{i}'][0]} {self.edge_map[f'C{i}'][1]} 100meg\n"
 
-            self.cmp_edg_str = self.cmp_edg_str + RC_str + "\n" + ".PARAM V0_value=1000\n"
+            self.cmp_edg_str += f"{RC_str}\n.PARAM V0_value=1000\n"
 
             for i in range(len(self.capacitor_val)):
-                self.cmp_edg_str = self.cmp_edg_str + ".PARAM C" + str(i) + "_value=" + str(self.capacitor_val[i]) + "\n"
+                self.cmp_edg_str += f".PARAM C{i}_value = {self.capacitor_val[i]}\n"
 
             for i in range(len(self.inductor_val)):
-                self.cmp_edg_str = self.cmp_edg_str + ".PARAM L" + str(i) + "_value=" + str(self.inductor_val[i]) + "\n"
+                self.cmp_edg_str += f".PARAM L{i}_value = {self.inductor_val[i]}\n"
 
-            self.cmp_edg_str = (
-                self.cmp_edg_str
-                + ".PARAM R0_value=10\n\n.model Ideal_switch SW (Ron=1m Roff=10Meg Vt=0.5 Vh=0 Lser=0 Vser=0)\n.model Ideal_D D\n\n"
-            )
+            self.cmp_edg_str += ".PARAM R0_value = 10\n\n.model Ideal_switch SW (Ron=1m Roff=10Meg Vt=0.5 Vh=0 Lser=0 Vser=0)\n.model Ideal_D D\n\n"
 
-            self.cmp_edg_str = (
-                self.cmp_edg_str
-                + "V_GSref_D  gs_ref_D 0 pwl(0 1 {GS0_T1-10e-9} 1 {GS0_T1} 0 {GS0_T2-10e-9} 0 {GS0_Ts} 1) r=0\nV_GSref_Dc  gs_ref_Dc 0 pwl(0 0 {GS0_T1-10e-9} 0 {GS0_T1} 1 {GS0_T2-10e-9} 1 {GS0_Ts} 0) r=0\n"
-            )
-            for i in range(len(self.switch_T1)):
-                self.cmp_edg_str = (
-                    self.cmp_edg_str
-                    + "V_GS"
-                    + str(i)
-                    + " GS"
-                    + str(i)
-                    + " 0 pwl(0 {GS"
-                    + str(i)
-                    + "_L1} {GS"
-                    + str(i)
-                    + "_T1-10e-9} {GS"
-                    + str(i)
-                    + "_L1} {GS"
-                    + str(i)
-                    + "_T1} {GS"
-                    + str(i)
-                    + "_L2}"
-                    + " {GS"
-                    + str(i)
-                    + "_T2-10e-9} {GS"
-                    + str(i)
-                    + "_L2} {GS"
-                    + str(i)
-                    + "_Ts} {GS"
-                    + str(i)
-                    + "_L1}) r=0\n"
-                )
+            self.cmp_edg_str += "V_GSref_D  gs_ref_D 0 pwl(0 1 {GS0_T1-10e-9} 1 {GS0_T1} 0 {GS0_T2-10e-9} 0 {GS0_Ts} 1) r=0\nV_GSref_Dc  gs_ref_Dc 0 pwl(0 0 {GS0_T1-10e-9} 0 {GS0_T1} 1 {GS0_T2-10e-9} 1 {GS0_Ts} 0) r=0\n"
 
             for i in range(len(self.switch_T1)):
-                self.cmp_edg_str = (
-                    self.cmp_edg_str
-                    + ".PARAM GS"
-                    + str(i)
-                    + "_Ts="
-                    + str(self.switch_T2[i] * 5)
-                    + "e-06\n"
-                    + ".PARAM GS"
-                    + str(i)
-                    + "_T1="
-                    + str(self.switch_T1[i] * 5)
-                    + "e-06\n"
-                    + ".PARAM GS"
-                    + str(i)
-                    + "_T2="
-                    + str(self.switch_T2[i] * 5)
-                    + "e-06\n"
-                    + ".PARAM GS"
-                    + str(i)
-                    + "_L1="
-                    + str(self.switch_L1[i])
-                    + "\n"
-                    + ".PARAM GS"
-                    + str(i)
-                    + "_L2="
-                    + str(self.switch_L2[i])
-                    + "\n"
-                )
+                self.cmp_edg_str += f"V_GS{i} GS{i} 0 pwl(0 {{GS{i}_L1}} {{GS{i}_T1-10e-9}} {{GS{i}_L1}} {{GS{i}_T1}} {{GS{i}_L2}} {{GS{i}_T2-10e-9}} {{GS{i}_L2}} {{GS{i}_Ts}} {{GS{i}_L1}}) r=0\n"
+
+            for i in range(len(self.switch_T1)):
+                self.cmp_edg_str += f".PARAM GS{i}_Ts = {self.switch_T2[i] * 5}e-06\n"
+                self.cmp_edg_str += f".PARAM GS{i}_T1 = {self.switch_T1[i] * 5}e-06\n"
+                self.cmp_edg_str += f".PARAM GS{i}_T2 = {self.switch_T2[i] * 5}e-06\n"
+                self.cmp_edg_str += f".PARAM GS{i}_L1 = {self.switch_L1[i]}\n"
+                self.cmp_edg_str += f".PARAM GS{i}_L2 = {self.switch_L2[i]}\n"
 
             if mode == "batch":
                 """
@@ -348,69 +288,52 @@ class PowerElectronics(Problem[npt.NDArray]):
                 .save @C0[i] @RC0[i] @C1[i] @RC1[i] @C2[i] @RC2[i] @C3[i] @RC3[i] @C4[i] @RC4[i] @C5[i] @RC5[i] @L0[i] @L1[i] @L2[i] @S0[i] @S1[i] @S2[i] @S3[i] @S4[i] @D0[i] @D1[i] @D2[i] @D3[i] @R0[i]
                 .save all
                 """
-                self.cmp_edg_str = self.cmp_edg_str + "\n.save "
+                self.cmp_edg_str += "\n.save "
                 for i in range(len(self.capacitor_val)):
-                    self.cmp_edg_str = self.cmp_edg_str + "@C" + str(i) + "[i] "
-                    self.cmp_edg_str = self.cmp_edg_str + "@RC" + str(i) + "[i] "
+                    self.cmp_edg_str += f"@C{i}[i] @RC{i}[i] "
                 for i in range(len(self.inductor_val)):
-                    self.cmp_edg_str = self.cmp_edg_str + "@L" + str(i) + "[i] "
+                    self.cmp_edg_str += f"@L{i}[i] "
                 for i in range(len(self.switch_T1)):
-                    self.cmp_edg_str = self.cmp_edg_str + "@S" + str(i) + "[i] "
+                    self.cmp_edg_str += f"@S{i}[i] "
                 for i in range(self.cmp_cnt["D"]):
-                    self.cmp_edg_str = self.cmp_edg_str + "@D" + str(i) + "[i] "
-                self.cmp_edg_str = (
-                    self.cmp_edg_str + "@R0[i] "
-                )  # This line should be outside the for loop, otherwise it will be duplicated.
-                self.cmp_edg_str = self.cmp_edg_str + "\n.save all\n"
+                    self.cmp_edg_str += f"@D{i}[i] "
+                self.cmp_edg_str += "@R0[i]\n"  # This line should be outside the for loop, otherwise it will be duplicated.
+                self.cmp_edg_str += ".save all\n"
 
-                self.cmp_edg_str = self.cmp_edg_str + ".tran 5n 1.06m 1m 5n uic\n"
+                self.cmp_edg_str += ".tran 5n 1.06m 1m 5n uic\n"
                 # The following .meas(ure) can be replaced by .print, although the former is tested and preferred, while the latter has not been tested in all cases.
-                self.cmp_edg_str = (
-                    self.cmp_edg_str
-                    + f".meas TRAN Vo_mean avg par('V({self.edge_map['R0'][0]}) - V({self.edge_map['R0'][1]})') from=1m to=1.06m\n"
-                )
+                self.cmp_edg_str += f".meas TRAN Vo_mean avg par('V({self.edge_map['R0'][0]}) - V({self.edge_map['R0'][1]})') from = 1m to = 1.06m\n"
                 self.cmp_edg_str = self.cmp_edg_str + ".meas TRAN gain param='Vo_mean/1000'\n"
-                self.cmp_edg_str = (
-                    self.cmp_edg_str
-                    + f".meas TRAN Vpp pp par('V({self.edge_map['R0'][0]}) - V({self.edge_map['R0'][1]})') from=1m to=1.06m\n"
-                )
-                self.cmp_edg_str = self.cmp_edg_str + ".meas TRAN Vpp_ratio param='Vpp/Vo_mean'\n"  # Ripple voltage
-                self.cmp_edg_str = self.cmp_edg_str + "\n.end"
+                self.cmp_edg_str += f".meas TRAN Vpp pp par('V({self.edge_map['R0'][0]}) - V({self.edge_map['R0'][1]})') from = 1m to = 1.06m\n"
+                self.cmp_edg_str += ".meas TRAN Vpp_ratio param = 'Vpp / Vo_mean'\n"  # Ripple voltage
+                self.cmp_edg_str += "\n.end"
 
             elif mode == "control":
-                self.cmp_edg_str = self.cmp_edg_str + "\n.control\nsave "
+                self.cmp_edg_str += "\n.control\nsave "
                 for i in range(len(self.capacitor_val)):
-                    self.cmp_edg_str = self.cmp_edg_str + "@C" + str(i) + "[i] "
-                    self.cmp_edg_str = self.cmp_edg_str + "@RC" + str(i) + "[i] "
+                    self.cmp_edg_str += f"@C{i}[i] @RC{i}[i] "
                 for i in range(len(self.inductor_val)):
-                    self.cmp_edg_str = self.cmp_edg_str + "@L" + str(i) + "[i] "
+                    self.cmp_edg_str += f"@L{i}[i] "
                 for i in range(len(self.switch_T1)):
-                    self.cmp_edg_str = self.cmp_edg_str + "@S" + str(i) + "[i] "
+                    self.cmp_edg_str += f"@S{i}[i] "
                 for i in range(self.cmp_cnt["D"]):
-                    self.cmp_edg_str = self.cmp_edg_str + "@D" + str(i) + "[i] "
-                self.cmp_edg_str = (
-                    self.cmp_edg_str + "@R0[i] "
-                )  # This line should be outside the for loop, otherwise it will be duplicated.
-                self.cmp_edg_str = self.cmp_edg_str + "\nsave all\n"
+                    self.cmp_edg_str += f"@D{i}[i] "
+                self.cmp_edg_str += "@R0[i]\n"  # This line should be outside the for loop, otherwise it will be duplicated.
+                self.cmp_edg_str += "save all\n"
 
-                self.cmp_edg_str = self.cmp_edg_str + "tran 5n 1.06m 1m 5n uic\n"
-                self.cmp_edg_str = (
-                    self.cmp_edg_str + f"let Vdiff=V({self.edge_map['R0'][0]}) - V({self.edge_map['R0'][1]})\n"
-                )
-                self.cmp_edg_str = self.cmp_edg_str + "meas TRAN Vo_mean avg Vdiff from=1m to=1.06m\n"
-                self.cmp_edg_str = self.cmp_edg_str + "meas TRAN Vpp pp Vdiff from=1m to=1.06m\n"
-                self.cmp_edg_str = (
-                    self.cmp_edg_str
-                    + "let Gain=Vo_mean/1000\nlet Vpp_ratio=Vpp/Vo_mean\nprint Gain, Vpp_ratio\nrun\nset filetype=binary\n"
-                )
-                self.cmp_edg_str = self.cmp_edg_str + f"write {self.raw_file_path}\n"
+                self.cmp_edg_str += "tran 5n 1.06m 1m 5n uic\n"
+                self.cmp_edg_str += f"let Vdiff = V({self.edge_map['R0'][0]}) - V({self.edge_map['R0'][1]})\n"
+                self.cmp_edg_str += "meas TRAN Vo_mean avg Vdiff from = 1m to = 1.06m\n"
+                self.cmp_edg_str += "meas TRAN Vpp pp Vdiff from = 1m to = 1.06m\n"
+                self.cmp_edg_str += "let Gain = Vo_mean / 1000\nlet Vpp_ratio = Vpp / Vo_mean\nprint Gain, Vpp_ratio\nrun\nset filetype = binary\n"
+                self.cmp_edg_str += f"write {self.raw_file_path}\n"
 
-                self.cmp_edg_str = self.cmp_edg_str + "quit\n.endc\n\n.end"
+                self.cmp_edg_str += "quit\n.endc\n\n.end"
 
             file.write(self.cmp_edg_str)
             file.close()  # explicitly added this line to ensure that the rewritten netlist file is closed
 
-    def __calculate_efficiency(self, exe: bool = False) -> tuple[float, int, float, float]:
+    def __calculate_efficiency(self) -> tuple[float, int, float, float]:
         capacitor_model, inductor_model, switch_model, diode_model = [], [], [], []
         max_comp_size = 12
         Ts = 5e-6
@@ -503,9 +426,7 @@ class PowerElectronics(Problem[npt.NDArray]):
         """
         self.__process_topology(sweep_data=design_variable)
         self.__rewrite_netlist(mode="control")
-        Efficiency, error_report, _, _ = self.__calculate_efficiency(
-            exe=True
-        )  # TODO: Use True for Windows, False for Ubuntu
+        Efficiency, error_report, _, _ = self.__calculate_efficiency()
         print(f"Error report from _calculate_efficiency: {error_report}")
         DcGain, VoltageRipple = self.__parse_log_file()
         self.simulation_results = np.array([DcGain, VoltageRipple, Efficiency])
@@ -550,16 +471,44 @@ if __name__ == "__main__":
         0,
         0,
         1,
-        1,  # L1 values
+        1,  # GS_L1 values
         1,
         0,
         1,
         1,
-        0,  # L2 values
+        0,  # GS_L2 values
+    ]
+
+    # Simulate the problem with the provided design variable
+    problem.simulate(design_variable=sweep_data)
+    print(problem.simulation_results)  # [0.01244983 0.9094711  0.74045004]
+
+    # Another set of sweep data. C0 value and GS_L1, GS_L2 values are changed.
+    sweep_data = [
+        1.5485e-05,
+        0.00001948,
+        0.000015185,
+        0.000002442,
+        0.000009287,
+        0.000015377,  # C values
+        0.000354659,
+        0.000706596,
+        0.000195361,  # L values
+        0.867615857,  # T1
+        1,
+        1,
+        0,
+        0,
+        1,  # GS_L1 values
+        1,
+        1,
+        1,
+        0,
+        0,  # GS_L2 values
     ]
 
     # Simulate the problem with the provided design variable
     problem.simulate(design_variable=sweep_data)
     print(problem.simulation_results)  # [-1.27858   -0.025081   0.7827396]
 
-    # problem.render()
+    problem.render()
