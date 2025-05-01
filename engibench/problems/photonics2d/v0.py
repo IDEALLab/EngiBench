af62fd9c19b3f6b3092e07a5b30a6a88e6cca507
cashend <cashen.diniz123@gmail.com>
2025-05-01 13:55:48 -0400

    photonics random_design

--- engibench/problems/photonics2d/v0.py
+++ engibench/problems/photonics2d/v0.py
@@ -665,7 +665,8 @@ class Photonics2D(Problem[npt.NDArray]):
 
         return rho_start.astype(np.float32)
 
-    def random_design(self, noise: float | None = None, blur: int = 0) -> tuple[npt.NDArray, int]:
+
+    def random_design(self, noise: float | None = None, blur: int = 0, dataset_key: str = "train", design_key: str = "optimal_design") -> tuple[npt.NDArray, int]:
         """Generates a random initial design.
 
         Can return a design with small random variations or a uniform design, or can pull
@@ -674,6 +675,8 @@ class Photonics2D(Problem[npt.NDArray]):
         Args:
             noise (float|None): If None, pull from dataset. If float, use that as the noise level.
             blur (int): The amount of pixel blurring to apply to random field. Only active if noise is used.
+            dataset_key (str): The key for the dataset to sample from.
+            design_key (str): The key for the design to sample from.
 
         Returns:
             tuple[npt.NDArray, int]: The starting design array (rho) and an integer (0).
@@ -685,8 +688,8 @@ class Photonics2D(Problem[npt.NDArray]):
         if noise is not None:
             rho_start = self._randomized_noise_field_design(noise=noise, blur=blur)
             return rho_start, 0
-        rnd = self.np_random.integers(low=0, high=len(self.dataset["train"]), dtype=int)
-        return np.array(self.dataset["train"]["optimal_design"][rnd]), rnd
+        rnd = self.np_random.integers(low=0, high=len(self.dataset[dataset_key]), dtype=int)
+        return np.array(self.dataset[dataset_key][design_key][rnd]), rnd
 
     def reset(self, seed: int | None = None, **kwargs) -> None:
         """Resets the problem, which in this case, is just the random seed."""
